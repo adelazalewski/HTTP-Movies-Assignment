27ecd699583ee0008cbb6da075c87a89fc6a9a07
adelazalewski <paliuadela@yahoo.com>
2020-11-03 21:33:30 -0500

    finished delete request

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
 function Movie({ addToSavedList }) {
   const [movie, setMovie] = useState(null);
+  const [successMessage, setSuccessMessage] = useState("")
   const params = useParams();
-
+  const {push} = useHistory();
+  console.log(params);
+console.log(movie);
   const fetchMovie = (id) => {
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
@@ -25,7 +28,20 @@ function Movie({ addToSavedList }) {
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
+const deleteMovie = (e) => {
 
+axios.delete(`http://localhost:5000/api/movies/${params.id}`)
+.then((res) => {
+  console.log("delete request res: ",res);
+  
+  setSuccessMessage(res.statusText);
+  
+push("/");
+})
+.catch(err => {
+  console.log(err);
+})
+}
   return (
     <div className="save-wrapper">
       <MovieCard movie={movie} />
@@ -33,6 +49,13 @@ function Movie({ addToSavedList }) {
       <div className="save-button" onClick={saveMovie}>
         Save
       </div>
+      <button>
+        Update Movie
+      </button>
+      <button onClick={deleteMovie}>
+        Delete Movie
+      </button>
+      {successMessage ? <p>Delate: {successMessage}</p> : null}
     </div>
   );
 }
---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Form from "./Form";
+
+export default function UpdateMovie() {
+    return (
+        <>
+        <Form />
+        <button>Update Movie</button>
+        </>
+    )
+}
